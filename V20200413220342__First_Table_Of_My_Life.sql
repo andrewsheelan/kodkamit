79b36789f73ad81169dc95e0a36898aa539c9080
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2020-07-27 11:49:12 -0400

    Add My First Table

---
+++ V20200413220342__First_Table_Of_My_Life.sql
@@ -0,0 +1,9 @@
+create table First_Table_Of_My_Life (
+    Life_Id int,
+    Life_Name varchar(255),
+    No_Of_Lifetimes int,
+    Days_Of_Ignorance int,
+    Tables_Left_To_Create int,
+    Created_At DateTime,
+    Updated_At DateTime 
+);
