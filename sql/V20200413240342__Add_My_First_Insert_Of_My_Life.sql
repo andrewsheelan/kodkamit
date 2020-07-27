420753b54406b4175e46b65500e13efa491308d7
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2020-07-27 12:09:18 -0400

    Add insert sql to sql folder

---
+++ sql/V20200413240342__Add_My_First_Insert_Of_My_Life.sql
@@ -0,0 +1,6 @@
+INSERT INTO First_Table_Of_My_Life
+VALUES (
+  1, 'Tom B. Erichsen', 10000, 7000, 4006,
+  convert(datetime,'20-06-12 10:34:09 PM',5),
+  convert(datetime,'20-06-12 10:34:09 PM',5)
+);
